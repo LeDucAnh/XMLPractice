d6d4b53d2cdbd02af06a49cdc581f48bc941f05a
Mac <ohdude10101995@yahoo.com>
2017-01-26 18:14:57 +0700

    finish project

--- XMLPractice/ViewController.swift
+++ XMLPractice/ViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 import Fuzi
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDataSource,UITableViewDelegate{
 
+    @IBOutlet weak var tableView: UITableView!
+    var OrderArray = [XMLPOrder]()
     func readFormXMLFile()
     {
         
         // Do any additional setup after loading the view, typically from a nib.
-        
+        //read form xml file 
         let file = "BaiTap_2" //this is the file. we will write to and read from it
         
         
@@ -31,6 +33,7 @@ class ViewController: UIViewController {
             do {
                 // if encoding is omitted, it defaults to NSUTF8StringEncoding
                 let document = try XMLDocument(string: xml, encoding: String.Encoding.utf8)
+
                 
                 //element
                 for element in document.xpath("//customer") {
@@ -38,18 +41,21 @@ class ViewController: UIViewController {
                     //  print(element.stringValue)
                     print("Email")
                     
-                    print(element.document.xpath("//emailCus").first?.stringValue)
+                   
                     
                 }
+                for element in document.xpath("//orderList/order") {
+
+                    let order = XMLPOrder(xmlDoc: element)
+                    self.OrderArray.append(order)
+
                 
-                
+                }
                 if let root = document.root {
                     print(root.tag)
                     // iterate through all children
                     for element in root.children {
-                        print("\(index) \(element.tag): \(element.attributes)")
-                        //  print(element.numberValue)
-                        print(element.stringValue)
+ 
                         
                     }
                     
@@ -75,11 +81,46 @@ class ViewController: UIViewController {
         
 
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.readFormXMLFile()
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.dataSource =  self
+        tableView.delegate =  self
     }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+     tableView.deselectRow(at: indexPath, animated: true)
 
+        let alert = UIAlertController(title: "Alert", message: self.OrderArray[indexPath.row].reportString(), preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return OrderArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+
+        var TitleString = "#Order " + String(indexPath.row + 1) + " "
+        
+        let attString = NSMutableAttributedString(string: "")
+        attString.append(NSMutableAttributedString(string: TitleString, attributes: [NSFontAttributeName:UIFont.boldSystemFont(ofSize: 17.0),NSForegroundColorAttributeName:UIColor.brown]))
+        
+        TitleString = "by " + " @" + String(self.OrderArray[indexPath.row].firstName) + String(self.OrderArray[indexPath.row].lastName)
+        attString.append(NSMutableAttributedString(string: TitleString, attributes: [NSForegroundColorAttributeName:UIColor.gray]))
+        
+
+        
+        cell?.textLabel?.attributedText = attString
+        
+  //    cell?.textLabel?.text =   self.OrderArray[indexPath.row].firstName +   String(self.OrderArray[indexPath.row].calcuateTotalPrice())
+        return  cell!
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- XMLPractice/XMLPOrder.swift
+++ XMLPractice/XMLPOrder.swift
@@ -8,46 +8,78 @@
 
 import UIKit
 import Fuzi
-
+struct XMLPOrderItem
+{
+    var quantity: Int // no need (!). It will be initialised from controller
+    var price : Double
+    var nameItem:String
+    var currency :String
+}
 class XMLPOrder: NSObject {
     var quantity: Int // no need (!). It will be initialised from controller
     var price : Double
     var nameItem:String
     var lastName:String
     var firstName:String
-    init(fromString fname: String,quantity:Int,nameItem:String,price:Double,lastName:String) {
+    var itemsArray = [XMLPOrderItem]()
+    init(fromString fname: String,quantity:Int,nameItem:String,price:Double,lastName:String,itemArray:[XMLPOrderItem]) {
         self.firstName = fname
         self.quantity = quantity
         self.lastName = lastName
         self.price =  price
         self.nameItem = nameItem
+        self.itemsArray = itemArray
         super.init()
     }
       convenience init(xmlDoc:XMLElement) {
-        let quantity: Int = Int((xmlDoc.document.xpath("//quantity").first?.numberValue)!) // no need (!). It will be initialised from controller
-        let price : Double =  Double((xmlDoc.document.xpath("//price").first?.numberValue)!)
-
-        let nameItem:String = (xmlDoc.document.xpath("//nameItem").first?.stringValue)!
-
-        let lastName:String = (xmlDoc.document.xpath("//lastName").first?.stringValue)!
-        let firstName:String = (xmlDoc.document.xpath("//firstName").first?.stringValue)!
-    
-       // for att in xmlDoc.attributes{
-        
-        //}
+      //right
+  //      print(xmlDoc.children(tag: "customer").first?.children(tag: "emailCus").first?.stringValue)
+   //wrong
+   // print(xmlDoc.children(tag: "customer").first?.document.xpath("//firstName").first?.stringValue)
 
         
-      //  print(element.document.xpath("//emailCus").first?.stringValue)
+       
+        var  itemsArray = [XMLPOrderItem]()
         
+        for element in xmlDoc.children(tag: "item") {
+            
+            
+            let newItem = XMLPOrderItem(quantity: element.children(tag: "quantity").first?.numberValue as! Int, price: element.children(tag: "price").first?.numberValue as! Double, nameItem: (element.children(tag: "nameItem").first?.stringValue)!,currency : (element.children(tag: "price").first?.attr("currency"))!)
+            //print(element.children(tag: "nameItem").first?.stringValue)
+     itemsArray.append(newItem)
+        
+        }
+        let quantity: Int = Int((xmlDoc.children(tag: "item").first?.children(tag: "quantity").first?.numberValue)!) // no need (!). It will be initialised from controller
+        let price : Double =  Double((xmlDoc.children(tag: "item").first?.children(tag: "price").first?.numberValue)!)
 
-        self.init(fromString : firstName,quantity:quantity,nameItem:nameItem,price:price,lastName:lastName)
-        ////   self.init(fromString:"John") // calls above mentioned controller with default name
+        let nameItem:String =  (xmlDoc.children(tag: "item").first?.children(tag: "nameItem").first?.stringValue)!
+
+        let lastName:String = (xmlDoc.children(tag: "customer").first?.children(tag: "lastName").first?.stringValue)!
+        let firstName:String = (xmlDoc.children(tag: "customer").first?.children(tag: "firstName").first?.stringValue)!
+        self.init(fromString : firstName,quantity:quantity,nameItem:nameItem,price:price,lastName:lastName,itemArray:itemsArray)
     }
-
     convenience override init() {
-           self.init(fromString : "asdfsa",quantity:-1,nameItem:"",price:-1,lastName:"")
+           self.init(fromString : "asdfsa",quantity:-1,nameItem:"",price:-1,lastName:"",itemArray:[XMLPOrderItem]())
      //   self.init(fromString:"John") // calls above mentioned controller with default name
     }
-
+    func calcuateTotalPrice()->Double
+    {
+        var cost = 0.0
+        for item in self.itemsArray
+        {
+           let tempCost =  item.price * Double(item.quantity)
+            cost += tempCost
+        }
+        
+        return cost
+    }
+    func reportString()->String
+    {
+        var string = "@" +  self.firstName + " : " + String(self.calcuateTotalPrice()) + " "
+   string +=   (self.itemsArray.first?.currency)!
+        return string
+        
+    }
+    
     
 }
